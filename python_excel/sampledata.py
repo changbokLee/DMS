2cbed57e85f7178fe183fbf706ac504d309507d1
changboklee <changbok2453@gmail.com>
2019-11-12 16:17:17 +0900

    샘플 코드만드는중

--- python_excel/sampledata.py
+++ python_excel/sampledata.py
@@ -1,12 +1,40 @@
 from openpyxl import Workbook
 from openpyxl.worksheet.table import Table, TableStyleInfo
+from openpyxl.chart import(
+    AreaChart,
+    Reference,
+    Series
+)
 
-wb = Workbook() # 워크북 생성
-ws = wb.active # active 속성 얻음
+wb = Workbook() # 워크북 만듬
+ws = wb.create_sheet() # sheet 생성
+ws.title = 'Fruit' 
+
 data = [
     ['Apples', 10000, 5000, 8000, 6000],
     ['Pears',   2000, 3000, 4000, 5000],
     ['Bananas', 6000, 6000, 6500, 6000],
     ['Oranges',  500,  300,  200,  700],
 ]
-wb.save('sampledata.xlsx')
+
+# 헤더 추가함
+ws.append(["Fruit", "2011", "2012", "2013", "2014"])
+for row in data:
+    ws.append(row)
+
+tab = Table(displayName="Table1", ref="A1:E5")
+
+# 테이블 스타일 적용
+style = TableStyleInfo(name="TableStyleMedium9", showFirstColumn=False,
+                       showLastColumn=False, showRowStripes=True, showColumnStripes=True)
+tab.tableStyleInfo = style
+ws.add_table(tab)
+
+# 차트 만들기
+chart = AreaChart()
+chart.title ="Fruit"
+chart.style= 13
+chart.x_axis.title ="Years"
+chart.y_axis.title = "Fruit"
+
+wb.save(filename ="table.xlsx") 
